bc60d888ed73df02c2ca57f0d3a453aeddd737d4
Hamed <amzhamed@gmail.com>
2018-12-21 16:54:59 +0330

    created twit model

--- twitter/models.py
+++ twitter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Twit(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=500)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
