307558df8c320a9045d1c887a61adff9fd80d9c2
sfch1999 <sor100100@yahoo.com>
2018-12-22 10:46:18 +0330

    1

--- twitter/views.py
+++ twitter/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from django.contrib.auth.decorators import login_required
 from .models import Twit
 
 
+@login_required
 def index(request):
     latest_twit_list = Twit.objects.order_by('-pub_date')
     template = loader.get_template('twitter/index.html')
