f9986c0ca6d714e8a4fb6100d9aead62dfc15222
sfch1999 <sor100100@yahoo.com>
2018-12-22 11:19:51 +0330

    BIG MISTAKE

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path,include
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/signup/', views.SignUp.as_view(), name='signup'),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -2,7 +2,15 @@ from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.decorators import login_required
 from .models import Twit
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
 
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'twitter/signup.html'
 
 @login_required
 def index(request):
