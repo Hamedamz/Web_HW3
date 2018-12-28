f5f3ab18bbf3c714448087b1a9446e84e18f87c0
sfch1999 <sor100100@yahoo.com>
2018-12-28 22:27:55 +0330

    section 3 completed

--- ids/models.py
+++ ids/models.py
@@ -5,4 +5,7 @@ class Report(models.Model):
     remote_address = models.GenericIPAddressField()
     user_agent = models.CharField(max_length=500)
     date_time = models.DateTimeField(auto_now_add=True)
+    n=models.IntegerField(default=0)
+    nn=models.IntegerField(default=0)
+
 
